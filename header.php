d0d4ada16f0d4dfc83274de05447a7eb2637f9b3
katou3891 <katia.rd@netcourrier.com>
2016-09-14 14:08:20 +0200

    Création du header.php
    Séparation du header du fichier accueil.php et modification de la navbar (ajout d'un calendrier etc...)

---
+++ header.php
@@ -0,0 +1,56 @@
+  <div class="row header">
+            <div class="container-fluid">
+                <nav class="navbar col-lg-12">
+                    <div class="container-fluid">
+                        <div class="navbar-header">
+                            <div class="navbar-brand"><img src="http://associations.it-sudparis.eu/images/logos/code.jpg" alt="logo" id="logo_header"/>Club Code_</div>
+                        </div>
+                        <ul class="nav navbar-nav navbar-right">
+                            <li class="active"><a href="accueil.php">Accueil</a></li>
+<!-- <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Qui sommes-nous ? <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="presentation.php">Présentation »</a></li>
+                                        <li><a href="https://www.facebook.com/profile.php?id=100009807875860&fref=ts">Notre page Facebook »</a></li>
+                                       
+                                    </ul>
+                                </li> -->
+                            <li><a href="./presentation.php">Présentation</a></li>
+                            <li><a href="./calendrier.php">Calendrier</a></li>
+
+
+ <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Activités <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="./ateliersbis.php">Ateliers »</a></li>
+                                        <li><a href="./soirees_code.php">Les soirées Code »</a></li>
+                                        <li><a href="./MakeITmagic.php">MakeItMagic »</a></li>
+                                    </ul>
+                                </li>
+                                <li class="dropdown">
+                                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Projets <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="./MakeITmagic.php">MakeItMagic »</a></li>
+                                        <li><a href="#">Conférence "Apprendre à programmer sans ...." »</a></li>
+                                    </ul>
+                                </li>
+
+ <li class="dropdown"><a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">????? <span class="caret"></span></a>
+                                    <ul class="dropdown-menu">
+                                        <li><a href="#">Notions de bases »</a></li>
+                                        <li><a href="#">Logiciels conseillés »</a></li>
+<li><a href="#">Des conseils pour s'organiser »</a></li>
+                                    </ul>
+                                </li>
+
+                                <li><a href="contact.php">Contact</a></li>     
+                                <li><a href="" title="Gestion" onclick="alert('Dirigerais vers une éventuelle page de connexion pour gérer les articles');"><span class="glyphicon glyphicon-lock"></span></a></li>
+                           
+
+
+
+                         
+                           
+                        </ul>
+                    </div> 
+                </nav>
+            </div>
+            </div>
