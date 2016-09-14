33b6e79132c644110f1c8995b9965aa9475f2e57
katou3891 <katia.rd@netcourrier.com>
2016-09-14 14:15:40 +0200

    Création du calendrier
    Attention : le calendrier afficher est un calendrier google mais n'est pas celui du club Code!

---
+++ calendrier.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Club Code</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Club Code, Télécom & Management SudParis">
+        <meta name="keywords" content="HTML, CSS, JS, JavaScript, bootstrap, club code, telecom management sudparis">
+        <meta name="author" content=""> 
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" type="text/css" rel="stylesheet" /><link href="css/calendrier.css" type="text/css" rel="stylesheet" />
+        
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+        
+        <script>gcal$perf$serverTime=32;gcal$perf$headStartTime=new Date().getTime()</script>
+
+<link type="text/css" rel="stylesheet" href="//calendar.google.com/calendar/static/d5586b1ef519fe7e232e348dec2a0238embedcompiled_fastui.css">
+
+
+<script type="text/javascript" src="//calendar.google.com/calendar/static/d5586b1ef519fe7e232e348dec2a0238embedcompiled__fr.js"></script>
+<script>function _onload() {window._init({"features":[0,0,0,1,1,1,1,1,0,0,0,1,1,0,1,0,1,1,0,1,0,1,0,0,0,0,0,1,0,0,0,1,0,1,1,0,0,1,0,0,1,1,0,0,0,0,1,1,0,1,0,0,1,1,0,0,1,1,0,1,1,0,1,0,0,1,1,1,1,1,0,0,0,1,0,0,1,0,0],"loggedin":true,"cids":{"h5cjaco241ocklbhknrrmei4cg@group.calendar.google.com":{"color":"#2952a3","access":20,"title":"Club Code;"}},"view":"month","weekstart":1,"format24hour":true,"dateFieldOrder":1,"preloadStart":"20160828","preloadEnd":"20161007","container":"container","baseUrl":"/","imagePath":"images/","timezone":"Europe/Paris","timezoneLocalized":"Paris","timezoneOffsetMs":7200000,"nowMs":"1473602762747","timezoneNextTransitionMs":"1477789200000","timezoneNextOffsetMs":3600000,"showNavigation":true,"showDateMarker":true,"showTabs":true,"showCalendarMenu":true,"showCurrentTime":true,"showTz":true,"showPrintButton":false,"showElementsLogo":true,"collapseAllday":false,"showSubscribeButton":true,"embedStyle":"","proxyUrl":"https://clients6.google.com","calendarApiVersion":"v3","developerKey":"AIzaSyBNlYH01_9Hc5S1J9vuFmu2nUqBZJNAXxs"});}</script>
+<script type="text/javascript">
+      var pageLoaded_ = false;
+      var clientLibraryLoaded_ = false;
+
+      function clientLibraryLoaded() {
+        clientLibraryLoaded_ = true;
+        if (pageLoaded_) _onload();
+      }
+
+      function pageLoaded() {
+        pageLoaded_ = true;
+        if (clientLibraryLoaded_) _onload();
+      }
+    </script>
+<script type="text/javascript" src="https://apis.google.com/js/client.js?onload=clientLibraryLoaded"></script>
+<style type="text/css">
+   
+  </style></head>
+<body onload="pageLoaded()">
+     <?php include("./header.php") ?>
+<h2 id="calendarTitle">Calendrier</h2>
+
+
+<div id="container"></div>
+     
+         <?php include("./footer.php") ?>
+    <noscript>
+Votre navigateur semble ne pas prendre en charge JavaScript. Cette page ne peut s'afficher correctement sans Javascript.
+Cette page est également disponible en version HTML à l'adresse :
+<a href="https://calendar.google.com/calendar/htmlembed?src=h5cjaco241ocklbhknrrmei4cg@group.calendar.google.com&amp;ctz=Europe/Paris">https://calendar.google.com/calendar/htmlembed?src=h5cjaco241ocklbhknrrmei4cg@group.calendar.google.com&amp;ctz=Europe/Paris</a></noscript>
+        
+    </body>
+</html>
