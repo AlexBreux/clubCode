70dafd504de791f19e01e8e7ea60c70c1adb01ff
katou3891 <katia.rd@netcourrier.com>
2016-09-14 14:43:54 +0200

    Creation de ateliers.php
    Présentation des ateliers

---
+++ ateliers.php
@@ -0,0 +1,155 @@
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+       <title>Club Code</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Club Code, Télécom & Management SudParis">
+        <meta name="keywords" content="HTML, CSS, JS, JavaScript, bootstrap, club code, telecom management sudparis">
+        <meta name="author" content=""> 
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" type="text/css" rel="stylesheet" />
+        
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+      
+        
+    <!-- Custom styles -->
+      
+        
+        
+         <link rel="stylesheet" href="style.css"  type="text/css">    
+         <link rel="stylesheet" href="ateliersbis.css"  type="text/css">    
+ 
+  </head>
+
+  <body>
+
+      
+      <?php include("./header.php") ?>
+  
+
+   
+
+    <div class="container">
+
+      <div class="row">
+      
+           <div class="col-xs-10">
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Titre atelier</h2>
+            <p class="blog-post-meta">1 Janvier 2014 par <a href="#">Mark</a> dans <a href="#">Développement Web</a></p>
+
+            <p>This blog post shows a few different types of content that's supported and styled with Bootstrap. Basic typography, images, and code are all supported.</p>
+            <hr>
+            <p>Cum sociis natoque penatibus et magnis ras mattis consectetur purus sit amet fermentum.</p>
+            <blockquote>
+              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </blockquote>
+           
+            <h2>Heading</h2>
+            <p>Viat eros.</p>
+            <h3>Sub-heading</h3>
+            <p>Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus.</p>
+            <pre><code>Example code block</code></pre>
+            <p><a class="btn btn-primary" href="#" role="button">Article complet &raquo;</a></p>
+       
+          </div><!-- /.blog-post -->
+           
+            
+
+          <div class="blog-post">
+            <h2 class="blog-post-title">Git</h2>
+            <p class="blog-post-meta">Octobre 1, 2016 par <a href="#">@elbichon</a> dans <a href="#">Développement Web</a></p>
+
+            <p><a href="#">dis parturient montes</a>, afin d'y présenter ..................e consectetur est at lobortis. Cras mattis consectetur purus sit amet fermentum.</p>
+            <blockquote>
+              <p>Curabitur blandit tempus porttitor. <strong>Nullam quis risus eget urna mollis</strong> ornare vel eu leo. Nullam id dolor id nibh ultricies vehicula ut id elit.</p>
+            </blockquote>
+            <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+               <p><a class="btn btn-primary" href="#" role="button">Article complet &raquo;</a></p>
+          </div><!-- /.blog-post -->
+
+            <div class="blog-post">
+            <h2 class="blog-post-title">??????</h2>
+            <p class="blog-post-meta">Août 25, 2016 par <a href="#">@elbichon</a> dans <a href="#">Développement Web</a></p>
+
+            <p> jzaadui bz iufbauidb iqbdnibndeiuz fuiezri nfiuzni bcibcjbjzduhd nudzoqdh oqh doh hqd h Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                 <p>Etiam porta <em>sem malesuada magna</em> mollis euismod. Cras mattis consectetur purus sit amet fermentum. Aenean lacinia bibendum nulla sed consectetur.</p>
+            <p>Vivamus sagittis lacus vel augue laoreet rutrum faucibus dolor auctor. Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros.</p>
+                 <p><a class="btn btn-primary" href="#" role="button">Article complet &raquo;</a></p>
+            
+          </div><!-- /.blog-post -->
+            
+
+          <nav class="blog-pagination">
+            <a class="btn btn-outline-primary" href="#">Précédent</a>
+            <a class="btn btn-outline-secondary disabled" href="#">Suivant</a>
+          </nav>
+
+        </div><!-- /.blog-main -->
+        
+       <div class="col-xs-2">
+          <div class="sidebar-module">
+            <h4>Les ateliers</h4>
+              <p> Organisés le jeudi après-midi blab bld nezo hduih oh </p>
+          </div>
+             
+             <div class="sidebar-module">
+            <h4>Barre de recherche</h4>
+            <form>
+  <input type="text" name="search" placeholder="Search..">
+</form>
+          </div>
+            <div class="sidebar-module">
+            <h4>Articles Récents</h4>
+            <ol class="list-unstyled">
+              <li></li>           
+            </ol>
+          </div>
+            <div class="sidebar-module">
+            <h4>Les plus visités</h4>
+            <ol class="list-unstyled">
+              <li></li>         
+            </ol>
+          </div>
+             <div class="sidebar-module">
+            <h4>Catégories</h4>
+            <ol class="list-unstyled">
+              <li><a href="">Toutes</a></li>
+              <li><a href="">Développement Web</a></li>
+              <li><a href="">Java</a></li>
+              <li><a href="">Révisions</a></li>
+            </ol>
+          </div>
+          <div class="sidebar-module">
+            <h4>Archives</h4>
+            <ol class="list-unstyled">
+              <li><a href="#">November 2016</a></li>
+              <li><a href="#">October 2016</a></li>
+              <li><a href="#">September 2016</a></li>
+              <li><a href="#">Août 2016</a></li>
+            </ol>
+          </div>
+         
+            
+           
+        </div><!-- /.blog-sidebar -->
+
+      </div><!-- /.row -->
+
+    </div><!-- /.container -->
+
+      <?php include("./footer.php") ?>
+
+
+ 
+  </body>
+</html>
