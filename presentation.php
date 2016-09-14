f90183dde1b497cd621d0d31c667b84699807550
katou3891 <katia.rd@netcourrier.com>
2016-09-14 14:21:26 +0200

    Create presentation.php
    Création de la page de présentation du club
    (Manque les noms des membres des équipes)

---
+++ presentation.php
@@ -0,0 +1,49 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Club Code</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Club Code, Télécom & Management SudParis">
+        <meta name="keywords" content="HTML, CSS, JS, JavaScript, bootstrap, club code, telecom management sudparis">
+        <meta name="author" content=""> 
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" type="text/css" rel="stylesheet" />
+        
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+      <?php include("./header.php") ?>
+<div class="container">
+        <h2>Présentation</h2>
+    <div class="row">
+<div class="col-sm-7">
+    
+                    <p>Le Club Code est le club de programmation de <a href="https://www.telecom-sudparis.eu" target="_blank">Télécom SudParis</a> et <a href="https://www.telecom-em.eu/">Télécom École de Management</a>. Ce club fait partie de l'association <a href="https:/minet.net" target="_blank">MiNET</a>, chargée de la gestion du réseau informatique chargée de la gestion du réseau informatique de la <acronym title="MAISon des ELèves">MAISEL</acronym>.<p>
+                    <p>groupe facebook</p>
+                   </div>
+    <div class="col-sm-5">
+    <img src="" alt="Equipe 2016-2017" />
+    </div>
+        </div>
+        
+        <h2>Équipe</h2>
+        <h3>Année 2016-2017</h3>
+        <p></p>
+    <h4>Collaborateurs</h4>
+           <h3>Année 2015-2016</h3>
+        <p></p>
+           <h3>Année 2014-2015</h3>
+        <p></p>
+           <h3>Année 2013-2014</h3>
+        <p></p>
+    
+        </div>
+              <?php include("./footer.php") ?>
+    </body>
+   </html>
