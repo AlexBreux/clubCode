8350ceaffbd75241fa50436fb87079dcbf9c7293
katou3891 <katia.rd@netcourrier.com>
2016-09-14 14:22:15 +0200

    Creation de la pae de présentation des soirée code

---
+++ soirees_code.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Club Code</title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta name="description" content="Club Code, Télécom & Management SudParis">
+        <meta name="keywords" content="HTML, CSS, JS, JavaScript, bootstrap, club code, telecom management sudparis">
+        <meta name="author" content=""> 
+        
+        <link href="css/bootstrap.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/bootstrap-theme.min.css" rel="stylesheet" type="text/css" />
+        <link href="css/style.css" type="text/css" rel="stylesheet" />
+        
+        <link rel="icon" type="image/x-icon" href="favicon.ico" />
+        
+        <script src="js/jquery.min.js" type="text/javascript"></script>
+        <script src="js/bootstrap.min.js" type="text/javascript"></script>
+    </head>
+    <body>
+      <?php include("./header.php") ?>
+         <!--   <div id="carousel" class="carousel slide">
+                <ol class="carousel-indicators">
+                    <li data-target="#carousel" data-slide-to="0" class="active"></li>
+                    <li data-target="#carousel" data-slide-to="1"></li>
+                    <li data-target="#carousel" data-slide-to="2"></li>
+                </ol>
+                <div class="carousel-inner" role="listbox">
+                    <div class="item active">
+                        <img class="first-slide" src="img/pic2.jpg" alt="Premier slide">
+                        <div class="container">
+                            <div class="carousel-caption">
+                                <h1>Bienvenue sur le site Web du Club Code</h1>
+                                <p class="carousel-caption-text"> Le Club Code est le club de programmation de <a href="https://www.telecom-sudparis.eu" target="_blank">Télécom SudParis</a> et <a href="https://www.telecom-em.eu/">Télécom École de Management</a>. </p>
+ <p class="carousel-caption-text"> Nous faisons....
+</p>
+                                <p><a class="btn btn-lg btn-primary" href="#" role="button">xxxxxxx »</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img class="second-slide" src="img/pic3.jpg" alt="Deuxieme slide">
+                        <div class="container">
+                            <div class="carousel-caption">
+                                <h1>Journée de clôture</h1>
+                                <p class="carousel-caption-text">Journée de cloture etc... En septembre une nouvelle équipe reprendra le projet etc...</p>
+                                <p><a class="btn btn-lg btn-info" href="#" role="button">En savoir plus »</a></p>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="item">
+                        <img class="third-slide" src="img/pic4.jpg" alt="Troisieme slide">
+                        <div class="container">
+                            <div class="carousel-caption">
+                                <h1>Nous contacter</h1>
+                                <p class="carousel-caption-text">Vous avez une question ou une remarque à nous faire ?</p>
+                                <p class="carousel-caption-text">Club Code<br />Télécom & Management SudParis<br />9 Rue Charles Fourier<br />91 011 Évry CEDEX</p>
+                                <p><a class="btn btn-lg btn-success" href="#" role="button">Contactez nous »</a></p>
+                            </div>
+                        </div>
+                    </div>
+
+                    <a class="left carousel-control" href="#carousel" role="button" data-slide="prev">
+                        <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+                        <span class="sr-only">Previous</span>
+                    </a>
+                    <a class="right carousel-control" href="#carousel" role="button" data-slide="next">
+                        <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+                        <span class="sr-only">Next</span>
+                    </a>
+                </div>
+            </div> -->
+        
+    
+            <div class="container">
+                <h1 class="page-header">Les soirées Code</h1>
+                <p>Organisées le .... Pour travailler en groupe sur un projet (scolaire ou non), ou bien pour travailler seul ..... </p>
+                
+                
+      <div class="row featurette">
+        
+
+    
+                 <h2 class="page-header">Actualités</h1>
+               
+              
+                <div style="margin-bottom: 20px;">
+                    <h3>LA première soirée code a lieu le ...</h3>
+                    <p>Que tu sois débutant ou ...</p>
+                   
+                </div>
+          <div style="margin-bottom: 20px;">
+                    <h3>Dernière soirée code</h3>
+                    <p>Retrouvez-nous en BL005 entre 20h et minuit jeudi ... pour la dernière soirée code de l'année</p>
+                   
+                </div>
+                
+            
+                 
+
+                </div>
+            </div>
+      
+      <?php include("./footer.php") ?>
+        <script>$(function () {  $('.carousel').carousel({ interval: 5000 }); });</script> <!-- Initialise le slider pour le défilement automatique, avec un interval donné, ici 5000ms -->
+    </body>
+</html>
